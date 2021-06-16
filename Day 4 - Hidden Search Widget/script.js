ecdef21be49990050092c443264581d71aaa05f3
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2021-06-16 09:10:54 +0300

    update

--- Day 4 - Hidden Search Widget/script.js
+++ Day 4 - Hidden Search Widget/script.js
@@ -4,6 +4,9 @@ const input = document.querySelector('.input')
 
 
 btn.addEventListener('click', function() {
+  // переключаем класс при нажатии на кноку Поиска путем метола toggle
   search.classList.toggle('active')
+
+  // сразу фиксируем курсор в поле инпута поиска
   input.focus()
 })
